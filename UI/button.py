f95ca7aab292f18bacce719a71be3f787885dbd2
Darsheel <darshgames.salecha@gmail.com>
2026-05-26 20:33:28 -0400

    Fixed some scaling issues for the UI

--- UI/button.py
+++ UI/button.py
@@ -5,15 +5,19 @@ class Button:
     def __init__(self,x,y,game,image,callback,scale=None):
         self.game = game
         self.image = image
-        self.rect = pygame.Rect(x, y, self.image.get_width(), self.image.get_height())
         self.callback = callback
         self.hovered = False
         self.scale = GLOBAL_SCALE if scale is None else scale
         self.image = self.scale_image(scale)
-        
+        self.rect = self.image.get_rect(topleft=(x,y))
+    
     def scale_image(self,scale):
         width,height = self.image.get_size()
-        return pygame.transform.scale(self.image, (int(width * scale), int(height * scale))) 
+        
+        new_width = int(width * scale)
+        new_height = int(height * scale)
+        
+        return pygame.transform.scale(self.image, (int(new_width), int(new_height))) 
 
     def update(self,event):
         mouse_pos = self.game.mouse_game_pos()
